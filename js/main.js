a995b7818cbd2756f0c46281243f753ce6a9abf4
GCaringi <giannicaringi94@gmail.com>
2022-05-19 15:10:34 +0200

    Add message, add image

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Hello Vue!",
+            imageUrl: "https://italiancoders.it/wp-content/uploads/2018/01/vue.png"
+        }
+    }
+)
